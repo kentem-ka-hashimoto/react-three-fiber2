3fd099e4898a4bb06c41ea54c7196fbbe6d7cd5e
ka-hashimoto <ka-hashimoto@kentem.co.jp>
2025-06-11 00:26:46 +0900

    音声入力した値を画面に文字列で表示するように実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,34 @@
+import { Canvas } from '@react-three/fiber'
+import { Text } from '@react-three/drei';
+import { useEffect, useState } from 'react';
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
+
 const App = () => {
+  const { transcript, resetTranscript } = useSpeechRecognition();
+  const [text, setText] = useState('');
+
+  useEffect(() => {
+    setText(transcript);
+  }, [transcript]);
+
+  if (!SpeechRecognition.browserSupportsSpeechRecognition()) {
+    return <span>ブラウザが音声認識をサポートしていません。</span>;
+  }
+
+  const handleStartListening = () => {
+    SpeechRecognition.startListening();
+  };
 
   return (
     <div>
-      Vite React
+      <button onClick={handleStartListening}>Start Listening</button>
+      <button onClick={resetTranscript}>Reset</button>
+      <Canvas>
+        <ambientLight />
+        <Text position={[0, 0, 0]} fontSize={1}>
+          {text}
+        </Text>
+      </Canvas>
     </div>
   )
 }
